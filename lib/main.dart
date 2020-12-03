3481bcd09cef3f920b823791a1f205f3b14659e2
NestorDevs <neivertjosias@gmail.com>
2020-12-03 11:55:11 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:band_names/services/socket_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'pages/home.dart';
+import 'pages/status.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SocketService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Band Names',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => HomePage(),
+          'status': (_) => StatusPage(),
+        },
+      ),
+    );
+  }
+}
